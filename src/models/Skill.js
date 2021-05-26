46b912bf9893f9575e35aca05cdbc90dbc0a7cd6
Lina Adamyan <adamyanlina97@gmail.com>
2021-05-27 02:05:13 +0400

    Add models: Employee, Position, Vacancy, Skill.

---
+++ src/models/Skill.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const idValidator = require('mongoose-id-validator');
+
+const { Schema } = mongoose;
+
+const Skill = new Schema({
+    skill_name: {
+        type: String,
+        required: true,
+    },
+    position_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'Position'
+    },
+    vacancy_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'Vacancy'
+    } 
+}, {
+    timestamps: true,
+    versionKey: false,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+});
+
+User.plugin(idValidator);
+
+module.exports = mongoose.model('Skill', Skill);
---
+++ src/models/Vacancy.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const idValidator = require('mongoose-id-validator');
+
+const { Schema } = mongoose;
+
+const Vacancy = new Schema({
+    vacancy_name: {
+        type: String,
+        required: true,
+    }
+}, {
+    timestamps: true,
+    versionKey: false,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+});
+
+Vacancy.plugin(idValidator);
+
+Position.virtual('skills', {
+    ref: 'Skill',
+    localField: '_id',
+    foreignField: 'vacancy_id',
+});
+
+module.exports = mongoose.model('Vacancy', Vacancy);
